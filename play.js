986553f2229c4115ff26dbd00ca8f258b252bccc
StephhyL <Stephanie Li>
2021-11-03 14:14:12 -0400

    added stdin.on('data', handleUserInput') as well as made handleUserInput function

--- play.js
+++ play.js
@@ -1,6 +1,24 @@
 
 const {connect} = require('./client');
 
+// setup interface to handle user input from stdin
+const setUpInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function () {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+
+  stdin.on("data", handleUserInput);
+  
+  return stdin;
+}
+
 
 console.log("Connecting....");
 connect(); // calling the connect function which has everything inside
