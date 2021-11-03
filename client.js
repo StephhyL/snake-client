71e0393aadcbb9f6d941f350cc3871e4ee31b4f8
StephhyL <Stephanie Li>
2021-11-03 14:01:01 -0400

    added movements to client;js

---
+++ client.js
@@ -0,0 +1,36 @@
+const net = require('net');
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243', // IP address
+    port: '50541' // PORT number
+  });
+
+  conn.setEncoding('utf8'); // interprets incoming data as text
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: PBJ");
+    let delay = 0;
+
+    const movement = ["Move: right", "Move: up", "Move: up", "Move: left", "Move: down"];
+    movement.forEach((movement) => {
+      setTimeout(() => {
+        conn.write(movement);
+      }, delay)
+      delay += 1000;
+    });
+  
+  })
+
+  // when I get data from the server, what to do with the data? in this case, console.logging it out
+  conn.on("data", (data) => {
+    console.log(data);
+  })
+
+  return conn;
+
+
+}
+
+module.exports = {connect};
