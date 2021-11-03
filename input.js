1f13c6335f53518e7672633c62225a56aec72900
StephhyL <Stephanie Li>
2021-11-03 14:22:28 -0400

    exported/imported setupInput

--- input.js
+++ input.js
@@ -1,16 +1,21 @@
-const setUpInput = function () {
+// setup interface to handle user input from stdin
+
+const handleUserInput = function () {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
-  const handleUserInput = function () {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-
   stdin.on("data", handleUserInput);
   
   return stdin;
-}
+};
+
+module.exports = {setupInput};
--- play.js
+++ play.js
@@ -1,24 +1,10 @@
 
 const {connect} = require('./client');
+const {setupInput} = require('./input')
 
-// setup interface to handle user input from stdin
-const setUpInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
 
-  const handleUserInput = function () {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-
-  stdin.on("data", handleUserInput);
-  
-  return stdin;
-}
 
 
 console.log("Connecting....");
 connect(); // calling the connect function which has everything inside
+setupInput(); // calling the setUpInput function
