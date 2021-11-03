aab7777b35b71333bd351f5a0c1edb7ed3028e91
StephhyL <Stephanie Li>
2021-11-03 13:39:27 -0400

    added name to snake using conn.write within the conn.on( - connect -

--- play.js
+++ play.js
@@ -1,24 +1,6 @@
-const net = require('net');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243', // IP address
-    port: '50541' // PORT number
-  });
+const {connect} = require('./client');
 
-  conn.setEncoding('utf8'); // interprets incoming data as text
-
-
-  // when I get data from the server, what to do with the data? in this case, console.logging it out
-  conn.on("data", (data) => {
-    console.log(data);
-  })
-
-  return conn;
-
-
-}
 
 console.log("Connecting....");
 connect(); // calling the connect function which has everything inside
